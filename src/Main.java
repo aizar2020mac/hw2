dbaf042d670f2f115a56d55c4f1ded0d83ac0465
aizar2020mac <osmonalievajzar@icloud.com>
2022-06-21 17:51:28 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(java(-0;15));
+        System.out.println(java(12;25));
+        System.out.println(java(7;21));
+        System.out.println(java(13;43));
+        System.out.println(java(19;37));
+
+
+    }
+    public static String java(int temperature , int age){
+    if (temperature >= -20 && temperature <= 30 && age >= 20 && age <=45) {
+    return "можно идти гулять";
+
+
+} else if (temperature >= 0 && temperature < 28 && age <=20 ){
+
+    } else {
+         return "оставайтесь дома";
+    }
+
+
+
